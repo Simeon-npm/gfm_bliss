1b25f99882abffabb8c2074073a62908d96e6c00
DevEmmy <eolaosebikan60@gmail.com>
2024-09-24 13:37:52 +0100

    removed quote thingy

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -8,7 +8,7 @@ const hero = () => {
         <h1 className='text-5xl sm:text-3xl font-bold'>Safe, Reliable, and Professional Electrical Services</h1>
         <p className='text-neutral-300 text-lg sm:text-sm w-[70%] sm:w-[85%]'>Licensed electricians available for installations, repairs, and emergency services. Serving your community with over 20 years of experience</p>
         <div className='space-x-5 sm:text-xs'>
-          <button className='px-5 py-3 bg-subordinate text-black font-semibold rounded'>Get a Free Quote</button>
+          {/* <button className='px-5 py-3 bg-subordinate text-black font-semibold rounded'>Get a Free Quote</button> */}
           <a href="#services" className='no-underline'>
             <button className='border-2 border-white px-5 py-3 rounded font-semibold'>Explore Services</button>
           </a>
