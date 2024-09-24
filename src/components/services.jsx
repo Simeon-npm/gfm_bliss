8fa60ee615c2378f85ee4a2867fb43e30b589f7b
Simeon <simeonthedeveloper@gmail.com>
2024-09-24 09:54:52 +0100

    services

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -3,11 +3,12 @@ import { servicesData } from '../constants'
 
 const services = () => {
   return (
-    <div>
-        <div className='grid grid-cols-4 sm:grid-cols-1'>
+    <div className='flex flex-col items-center py-20 gap-10'>
+        <h1 className='font-bold text-4xl'>Services</h1>
+        <div className='grid grid-cols-4 gap-5 sm:grid-cols-1 px-10'>
             {servicesData.map((item, index)=>(
-                <div className=' flex flex-col items-center px-10 text-center py-5 border'>
-                    <h1 className='text-xl font-semibold'>{item.title}</h1>
+                <div className=' flex flex-col items-center px-3 text-left py-5  gap-2 shadow-lg rounded'>
+                    <h1 className='text-xl font-semibold text-left'>{item.title}</h1>
                     <p className='text-sm'>{item.description}</p>
                 </div>
             ))}
